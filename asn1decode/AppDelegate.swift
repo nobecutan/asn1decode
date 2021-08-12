ad8153b4d66a4e4f54ce56155888cf698d893169
Christof Rath <christof.rath@prime-sign.com>
2021-08-12 10:02:23 +0200

    Initial Commit

---
+++ asn1decode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  asn1decode
+//
+//  Created by Christof Rath on 12.08.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
